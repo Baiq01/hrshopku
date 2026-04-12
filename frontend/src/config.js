beab44caf0026444d791679d3ebbe65771f044e2
Yazid <yaziddev04@gmail.com>
2026-04-13 01:16:46 +0700

    ubah config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = process.env.REACT_APP_API_BASE || 'http://127.0.0.1:8000/api';
+export const API_BASE = process.env.REACT_APP_API_BASE || 'hrshopkubajubodo.com/api';
 export const MIDTRANS_CLIENT_KEY = process.env.REACT_APP_MIDTRANS_CLIENT_KEY || '';
