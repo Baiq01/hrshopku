5b023ebd6829d8a8ae240380fc6c9d0ec2dc69e4
Yazid <yaziddev04@gmail.com>
2026-04-13 23:40:14 +0700

    Fix: Membersihkan folder sampah agar tidak ikut ke VPS

--- backend/config/product.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'allowed_sizes' => ['XS','S','M','L','XL','XXL','3XL','4XL','FREE'],
-];
--- frontend/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE = process.env.REACT_APP_API_BASE || 'https://hrshopkubajubodo.com/api';
-export const MIDTRANS_CLIENT_KEY = process.env.REACT_APP_MIDTRANS_CLIENT_KEY || '';
