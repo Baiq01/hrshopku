214073f4632ff8a2dcd1f73f60304c96e7acae58
Baiq Raihanah <baiqraihana4@gmail.com>
2026-04-21 09:49:08 +0800

    sanbox midtrans

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -10,7 +10,7 @@ function loadMidtrans(clientKey){
     if (document.getElementById('midtrans-script')) return resolve();
     const s = document.createElement('script');
     s.id = 'midtrans-script';
-    s.src = `https://app.midtrans.com/snap/snap.js`;
+    s.src = `https://app.sandbox.midtrans.com/snap/snap.js`;
     s.setAttribute('data-client-key', clientKey);
     s.onload = () => resolve();
     s.onerror = reject;
